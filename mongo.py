e9d17de9398dc7dae08bd29501cb052c809ebc3e
Rick Ferreira <ricardo.silva@sptrans.com.br>
2018-06-19 21:54:58 -0300

    aula 10

---
+++ mongo.py
@@ -0,0 +1,16 @@
+#!/usr/bin//python3
+
+from pymongo import MongoClient
+
+
+
+
+
+#client  = MongoClient('127.0.0.1')
+#db =  client['teste']
+#db.bilhetes.remove({"nome":"roberto"})
+#db.bilhetes.update({'_id':2},{"$set":{'sobrenome':'silva'}})
+#auxiliar =  [x for x in db.bilhetes.find()]
+#for auxiliar in db.bilhetes.find():
+    #for key,value in auxiliar.items():
+        #print ('{}:{}'.format(key,value))
